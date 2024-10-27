e9259d5834e4916afc1cd98e2a7555776b501ec1
skwich <a.3066@mail.ru>
2024-10-27 17:00:13 +0500

    prepare to complete tasks

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
-a = [[],[0,1,2],[4,5,6]]
+import time
 
-for i in a:
-    if len(i):
-        print(i[0])
+def log(is_get_time: bool):
+    def decorator(input_func):
+        def output_func(*args):
+            result = input_func(*args)
+            date = f"[{time.ctime()}] " if is_get_time else ""
+            print(f"{date}Func: {input_func.__name__}, Text: {result}")
+            return result
+        return output_func
+    return decorator
+
+@log(True)
+def print_text_with_time(text):
+    print(text)
+    return text
+
+@log(False)
+def print_text(text):
+    print(text)
+    return text
+
+print_text("POCOX3PRO")
+print_text_with_time("AKYLA")
