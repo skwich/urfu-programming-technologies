fce493f06c8d321ae5be1885b36cf5ba8fcfd2ec
skwich <a.3066@mail.ru>
2024-10-31 23:34:58 +0500

    complete task

--- test.py
+++ test.py
@@ -1,12 +1,63 @@
 def apply_filter(func):
-    def wrapper(*args, **kwargs):
-        print("=====")
-        print(args[0])
-        print("=====")
+    def wrapper(*args):
+        text = func(*args)
+        text = "biboba"
+        return text
     return wrapper
 
-@apply_filter
-def print_text(text, abc):
-    print(text, abc)
+def apply_sort(func):
+    def wrapper(*args):
+        text = func(*args)
+        text = "didoda"
+        return text
+    return wrapper
 
-print_text("aboba", 123)
+def print_formatted_text(func):
+    def wrapper(*args):
+        print(func(*args))
+    return wrapper
+
+@print_formatted_text
+@apply_sort
+@apply_filter
+def print_text(text):
+    return text
+
+# print_text("aboba")
+
+#---
+
+students = [
+    {
+        "alex": 20,
+        "vitya": 19
+    },
+    {
+        "petya": 23,
+        "vova": 24
+    }
+]
+
+# for item in students:
+#     for e in item.items():
+#         print(e)
+
+for student in students:
+    for (key,value) in student.items():
+        student[key] = "2000"
+
+# for item in students:
+#     for e in item.items():
+#         print(e)
+
+
+
+
+
+
+
+
+
+
+
+
