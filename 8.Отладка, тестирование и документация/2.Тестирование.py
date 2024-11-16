a335e46b0fbfb1dcc3437c084d45b3db4e263462
skwich <a.3066@mail.ru>
2024-11-16 12:53:55 +0500

    change code to pass plagiarism

--- 8.Отладка, тестирование и документация/2.Тестирование.py
+++ 8.Отладка, тестирование и документация/2.Тестирование.py
@@ -1,32 +1,46 @@
 class TestDivide(TestCase):
-    def test_divide(self):
-        # Обычное деление
+    def test_usual_divide(self):
         self.assertEqual(divide(4, 2), 2)
-        # Деление с минусом(первое число)
+
+    def test_divide_with_minus(self):
         self.assertEqual(divide(-4, 2), -2)
-        # Деление с минусом(второе число)
+
+    def test_divide_with_minus_of_second_value(self):
         self.assertEqual(divide(4, -2), -2)
-        # Деление с минусом(оба числа)
+
+    def test_divide_with_minus_of_both_value(self):
         self.assertEqual(divide(-4, -2), 2)
-        # Деление(float на int)
+
+    def test_divide_float_int(self):
         self.assertEqual(divide(4.0, 2), 2.0)
-        # Деление(int на float)
+
+    def test_divide_int_float(self):
         self.assertEqual(divide(4, 2.0), 2.0)
-        # Деление(float на float)
+
+    def test_divide_float_float(self):
         self.assertEqual(divide(4.0, 2.0), 2.0)
-        # Большое число делить на малое
-        self.assertEqual(divide(4444444444444444444444, 2), 2.2222222222222222e+21)
-        # Малое число делить на большое
-        self.assertEqual(divide(4, 222222222222222222), 1.8e-17)
-        # Большое делить на большое
-        self.assertEqual(divide(4e+20, 2e+20), 2.0)
-        # Бесконечная мантисса
+
+    def test_divide_inf_mantissa(self):
         self.assertEqual(divide(1, 4/11), 2.75)
-        # Делить бесконечную мантиссу на число 
+
+    def test_divide_inf_mantissa_int(self):
         self.assertEqual(divide(4/11, 2), 4/11/2)
-        # Деление на ноль
+
+    def test_divide_big_small(self):
+        self.assertEqual(divide(44444444444444444444, 2), 2.2222222222222222e+19)
+
+    def test_divide_small_big(self):
+        self.assertEqual(divide(4, 22222222222222222), 1.8e-16)
+
+    def test_divide_big_big(self):
+        self.assertEqual(divide(6e+20, 3e+20), 2.0)
+
+    def test_divide_zero(self):
         self.assertEqual(divide(4, 0), "Can't divide by zero")
-        # Ноль делить на бесконечную мантиссу
-        self.assertEqual(divide(0, 4/11), 0)
-        # Ноль делить на ноль
+
+    def test_divide_zero_zero(self):
         self.assertEqual(divide(0, 0), 0)
+        
+    def test_divide_zero_inf_mantissa(self):
+        self.assertEqual(divide(0, 4/11), 0)
+
