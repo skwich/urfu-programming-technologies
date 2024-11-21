1ae266ed5eb225fd158a84ca77f31dd7f79b0e13
skwich <a.3066@mail.ru>
2024-11-22 00:19:35 +0500

    test

--- 10.Модули и библиотеки/1.Работа с Excel.py
+++ 10.Модули и библиотеки/1.Работа с Excel.py
@@ -2,18 +2,29 @@ import pandas as pd
 import openpyxl as op
 
 
-def get_vacancies(filename):
-    return pd.read_csv(
-        "vacancies.csv", names=["name", "s_from", "s_to", "s_cur", "city", "data"]
-    )
+def get_vacancies(filename: str):
+    return pd.read_csv("vacancies.csv", names=["name", "s_from", "s_to", "s_cur", "city", "date"])
 
 
-def create_statistics_of_years(wb, vacs):
-    
+def create_statistics_of_years(wb: op.Workbook, vacs: pd.DataFrame):
+    wb.create_sheet("Статистика по годам", 0)
+    sheet = wb[wb.sheetnames[0]]
+
+    vacs["year"] = vacs["date"].apply(lambda x: x[:4])
+    vacs["s_avg"] = vacs[["s_from", "s_to"]].mean(1)
+    df = vacs.groupby("year", as_index=False).agg({"s_avg": "mean", "name": "count"})
+    df["s_avg"] = df["s_avg"].round().astype(int)
+
+    data = ["Год", "Средняя зарплата", "Количество вакансий"] + df.values.tolist()
+    for i, row in enumerate(data, 1):
+        year, salary, count = row
+        sheet[f"A{i}"] = year
+        sheet[f"B{i}"] = salary
+        sheet[f"C{i}"] = count
 
 
-def create_statistics_of_city(wb, vacs):
-    pass
+def create_statistics_of_city(wb: op.Workbook, vacs: pd.DataFrame):
+    wb.create_sheet("Статистика по городам")
 
 
 def create_report():
