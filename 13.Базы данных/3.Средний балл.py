98bbd519278cd9209bd0d1c992cc43eda22d9498
skwich <a.3066@mail.ru>
2024-12-14 15:05:09 +0500

    complete task

--- 13.Базы данных/3.Средний балл.py
+++ 13.Базы данных/3.Средний балл.py
@@ -7,7 +7,14 @@ mark_table = input()
 conn = sqlite3.connect(database)
 
 query = f"""
-    SELECT {table}.*, 
+    SELECT {table}.name, ROUND(AVG({mark_table}.mark), 0) AS average
     FROM {table}
-    INNER JOIN {mark_table} ON {table}.id = {mark_table}.id
+    JOIN {mark_table} ON {table}.id = {mark_table}.id
+    GROUP BY {table}.id
+    ORDER BY {table}.id
 """
+
+result = conn.execute(query).fetchall()
+
+for row in result:
+    print(f"{row[0]} {row[1]}")
